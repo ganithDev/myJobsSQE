33f716123aba546730c27dd3189bead3579c63c0
ganithDev <ganithperera05@gmail.com>
2017-05-06 12:13:32 +0530

    SQL DB script with latest modifications
    Added person_contact_mode, person_status, person_title and Person table related with those tables
    Added referee_relationship table relates with referee table.

--- mymscsqeprocv_db.sql
+++ mymscsqeprocv_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2017 at 03:04 PM
+-- Generation Time: May 06, 2017 at 06:37 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -160,7 +160,7 @@ INSERT INTO `job_title` (`idJobTitle`, `jobTitle`, `Sector_idSector`) VALUES
 CREATE TABLE `person` (
   `idPerson` int(10) UNSIGNED NOT NULL,
   `idUser` int(11) UNSIGNED NOT NULL,
-  `title` varchar(10) DEFAULT NULL,
+  `idperson_title` int(11) NOT NULL,
   `forename1` varchar(45) NOT NULL,
   `forename2` varchar(45) DEFAULT NULL,
   `surname` varchar(45) NOT NULL,
@@ -174,14 +174,14 @@ CREATE TABLE `person` (
   `female` tinyint(1) DEFAULT NULL COMMENT 'need to check whether there could be legal  issues collecting/using this info\n',
   `postcodeStart` varchar(10) DEFAULT NULL COMMENT 'first part of the the persons address postcode for distance calculation',
   `authorityToWorkStatement` varchar(255) DEFAULT NULL COMMENT 'List restrictions on work, e.g. max 20 hours per week (some non-EU residents)\nE.g. if not null it should be displayed',
-  `contactPreference` enum('Mobile','Landline','Email1','Email2') DEFAULT NULL COMMENT 'mobile\nlandline\nemail1 (username)\nemail2 (secondEmail)',
+  `idcontactPreference` int(11) NOT NULL,
   `noOfGcses` smallint(3) UNSIGNED DEFAULT '0',
   `gcseEnglishGrade` varchar(2) DEFAULT NULL,
   `gcseMathsGrade` varchar(2) DEFAULT NULL,
   `fiveOrMoreGcses` tinyint(1) DEFAULT NULL,
   `noOfAlevels` smallint(4) DEFAULT '0' COMMENT 'entered by the user - could be checked against the number in the Alevels table.\nAS level is 0.5.  What about International Baccalaureate, French Baccalaureate,European Baccalaureate,Scottish Highers etc...',
   `ucasPoints` smallint(4) DEFAULT '0' COMMENT 'needed?',
-  `studentStatus` enum('Full-time','Part-time','Not a student') DEFAULT NULL COMMENT 'if a student then the details of the course should be stored in the Educational_qualifications table',
+  `idperson_status` int(11) NOT NULL,
   `mobile` varchar(16) DEFAULT NULL,
   `landline` varchar(16) DEFAULT NULL,
   `dob` date DEFAULT NULL COMMENT 'date of birth - may be required by some employes but cannot be searched on',
@@ -193,10 +193,72 @@ CREATE TABLE `person` (
 -- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`idPerson`, `idUser`, `title`, `forename1`, `forename2`, `surname`, `addressLine1`, `addressLine2`, `town`, `postcode`, `secondEmail`, `personalUrl`, `photo`, `female`, `postcodeStart`, `authorityToWorkStatement`, `contactPreference`, `noOfGcses`, `gcseEnglishGrade`, `gcseMathsGrade`, `fiveOrMoreGcses`, `noOfAlevels`, `ucasPoints`, `studentStatus`, `mobile`, `landline`, `dob`, `penaltyPoints`, `idLevelOfEmployment`) VALUES
-(1, 1, 'Mrs', 'ganith', 'dithula', 'perera', '71, 1 lane', 'Katubadde', 'Moratuwa', '10400', 'gani@ya.com', 'null', 'SKLL0274_EDT.JPG', 0, 'SL', 'authorityToWorkStatement', 'Mobile', 5, 'A', 'B', 0, 2, 10, 'Full-time', '0712281744', '0112587744', '1991-09-21', 11, 1),
-(2, 3, 'Mrs', 'fsd', 'dgdf', 'dgdf', 'dfgdf', 'dgdf', 'dfgd', '10230', 'naduni@kljlj', 'null', 'cat-modern-home-design.jpg', 0, 'SL', 'authorityToWorkStatement', 'Mobile', 5, 'A', 'B', 0, 2, 10, 'Full-time', '0719995375', '0112749145', '1991-02-07', 11, 1),
-(3, 4, 'Mrs', 'Kamal', 'Nisala', 'Perera', '103/1,', 'Colombo 04', 'Bambalapitiy', '10150', 'kamal@gmail.com', 'null', '87573__Interior - New - myname - Design.jpg', 0, 'SL', 'authorityToWorkStatement', 'Mobile', 5, 'A', 'B', 0, 2, 10, 'Full-time', '0718895375', '0112849145', '1991-02-07', 11, 1);
+INSERT INTO `person` (`idPerson`, `idUser`, `idperson_title`, `forename1`, `forename2`, `surname`, `addressLine1`, `addressLine2`, `town`, `postcode`, `secondEmail`, `personalUrl`, `photo`, `female`, `postcodeStart`, `authorityToWorkStatement`, `idcontactPreference`, `noOfGcses`, `gcseEnglishGrade`, `gcseMathsGrade`, `fiveOrMoreGcses`, `noOfAlevels`, `ucasPoints`, `idperson_status`, `mobile`, `landline`, `dob`, `penaltyPoints`, `idLevelOfEmployment`) VALUES
+(1, 1, 1, 'ganith', 'dithula', 'perera', '71, 1 lane', 'Katubadde', 'Moratuwa', '10400', 'gani@ya.com', 'null', 'SKLL0274_EDT.JPG', 0, 'SL', 'authorityToWorkStatement', 1, 5, 'A', 'B', 0, 2, 10, 2, '0712281744', '0112587744', '1991-09-21', 11, 1),
+(2, 2, 3, 'Naduni', '', 'Pulsarani', '12, Wolk street', '', 'Baybrook', '21331', 'hjk@gh.com', NULL, '', 0, 'SL', 'authorityToWorkStatement', 1, 5, 'A', 'B', 0, 2, 10, 1, '0758895588', '0112558877', '1991-06-28', 11, 1),
+(3, 3, 3, 'Naduni', '', 'Pulsarani', '12, Wolk street', '', 'Baybrook', '21331', 'hjk@gh.com', NULL, '', 0, 'SL', 'authorityToWorkStatement', 1, 5, 'A', 'B', 0, 2, 10, 1, '0758895588', '0112558877', '1991-06-28', 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person_contact_mode`
+--
+
+CREATE TABLE `person_contact_mode` (
+  `idcontactPreference` int(11) NOT NULL,
+  `contactMode` varchar(45) NOT NULL COMMENT '''Mobile'',''Landline'',''Email1'',''Email2''\nmobile\\nlandline\\nemail1 (username)\\nemail2 (secondEmail)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `person_contact_mode`
+--
+
+INSERT INTO `person_contact_mode` (`idcontactPreference`, `contactMode`) VALUES
+(1, 'Mobile'),
+(2, 'Email1'),
+(3, 'Email2'),
+(4, 'Landline');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person_status`
+--
+
+CREATE TABLE `person_status` (
+  `idperson_status` int(11) NOT NULL,
+  `person_status` varchar(45) DEFAULT NULL COMMENT 'if a student then the details of the course should be stored in the Educational_qualifications table'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `person_status`
+--
+
+INSERT INTO `person_status` (`idperson_status`, `person_status`) VALUES
+(1, 'Full-time'),
+(2, 'Part-time'),
+(3, 'Not a student');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person_title`
+--
+
+CREATE TABLE `person_title` (
+  `idperson_title` int(11) NOT NULL,
+  `title` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `person_title`
+--
+
+INSERT INTO `person_title` (`idperson_title`, `title`) VALUES
+(1, 'Mr.'),
+(2, 'Mrs.'),
+(3, 'Miss'),
+(4, 'Ms');
 
 -- --------------------------------------------------------
 
@@ -230,13 +292,32 @@ CREATE TABLE `referee` (
   `surname` varchar(45) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `contactPhone` varchar(16) DEFAULT NULL,
-  `relationship` enum('employer','academic') DEFAULT NULL,
+  `idreferee_relationship` int(11) NOT NULL,
   `permissionToContact` tinyint(1) DEFAULT NULL COMMENT 'the person should get an agreement and tick this box to overwrite the default 0',
   `permissionToStoreDetail` tinyint(1) DEFAULT NULL COMMENT 'person should try to get an agreement and check - otherwise the record will be deleted',
   `verified` tinyint(1) DEFAULT NULL,
   `howVerified` varchar(45) DEFAULT NULL COMMENT 'referee contacted by email/phone/letter',
   `referenceDoc` text COMMENT 'uploaded scanned reference?'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='need to check data protection for referee data';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `referee_relationship`
+--
+
+CREATE TABLE `referee_relationship` (
+  `idreferee_relationship` int(11) NOT NULL,
+  `relationship` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `referee_relationship`
+--
+
+INSERT INTO `referee_relationship` (`idreferee_relationship`, `relationship`) VALUES
+(1, 'employer'),
+(2, 'academic');
 
 -- --------------------------------------------------------
 
@@ -295,6 +376,26 @@ INSERT INTO `skill` (`idSkill`, `skill_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `skill_level`
+--
+
+CREATE TABLE `skill_level` (
+  `idskill_level` int(11) NOT NULL,
+  `skill_level` varchar(45) DEFAULT NULL COMMENT '''Basic'',''Good'',''Excellent''\nsubjective - but may be crossreferenced to to ER tests'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `skill_level`
+--
+
+INSERT INTO `skill_level` (`idskill_level`, `skill_level`) VALUES
+(1, 'Basic'),
+(2, 'Good'),
+(3, 'Excellent');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `skill_person`
 --
 
@@ -302,7 +403,7 @@ CREATE TABLE `skill_person` (
   `idSkill_person` int(10) UNSIGNED NOT NULL,
   `idPerson` int(10) UNSIGNED NOT NULL,
   `idSkill` int(10) UNSIGNED NOT NULL,
-  `skillLevel` enum('Basic','Good','Excellent') DEFAULT NULL COMMENT 'subjective - but may be crossreferenced to to ER tests',
+  `idskill_level` int(11) NOT NULL,
   `verified` tinyint(1) DEFAULT NULL,
   `howVerified` varchar(45) DEFAULT NULL COMMENT 'reference, ER test or other evidence of competence'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -327,9 +428,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`idUser`, `username`, `password`, `registerDate`, `idUser_type`) VALUES
 (1, 'ganithperera@outlook.com', '123456', '2017-04-12 15:27:12', 1),
-(2, 'naduni@sfsd', 'aaaaaa', '2017-04-15 09:02:53', 1),
-(3, 'naduni@dgfd', 'aaaaaa', '2017-04-15 12:16:13', 1),
-(4, 'kamal@gmail.com', 'aaaaaa', '2017-04-15 13:24:03', 1);
+(2, 'naduni@gmail.com', '147741', '2017-05-06 03:49:41', 1),
+(3, 'naduni@il.com', '147741', '2017-05-06 03:58:37', 1);
 
 -- --------------------------------------------------------
 
@@ -406,7 +506,28 @@ ALTER TABLE `job_title`
 ALTER TABLE `person`
   ADD PRIMARY KEY (`idPerson`),
   ADD KEY `fk_person_employment_level1_idx` (`idLevelOfEmployment`),
-  ADD KEY `fk_person_user1_idx` (`idUser`);
+  ADD KEY `fk_person_user1_idx` (`idUser`),
+  ADD KEY `fk_person_person_title1_idx` (`idperson_title`),
+  ADD KEY `fk_person_person_status1_idx` (`idperson_status`),
+  ADD KEY `fk_person_person_contact_mode1_idx` (`idcontactPreference`);
+
+--
+-- Indexes for table `person_contact_mode`
+--
+ALTER TABLE `person_contact_mode`
+  ADD PRIMARY KEY (`idcontactPreference`);
+
+--
+-- Indexes for table `person_status`
+--
+ALTER TABLE `person_status`
+  ADD PRIMARY KEY (`idperson_status`);
+
+--
+-- Indexes for table `person_title`
+--
+ALTER TABLE `person_title`
+  ADD PRIMARY KEY (`idperson_title`);
 
 --
 -- Indexes for table `professional_qualification`
@@ -422,7 +543,14 @@ ALTER TABLE `professional_qualification`
 --
 ALTER TABLE `referee`
   ADD PRIMARY KEY (`idReferee`),
-  ADD KEY `fk_referee_person1_idx` (`idPerson`);
+  ADD KEY `fk_referee_person1_idx` (`idPerson`),
+  ADD KEY `fk_referee_referee_relationship1_idx` (`idreferee_relationship`);
+
+--
+-- Indexes for table `referee_relationship`
+--
+ALTER TABLE `referee_relationship`
+  ADD PRIMARY KEY (`idreferee_relationship`);
 
 --
 -- Indexes for table `sector`
@@ -437,12 +565,19 @@ ALTER TABLE `skill`
   ADD PRIMARY KEY (`idSkill`);
 
 --
+-- Indexes for table `skill_level`
+--
+ALTER TABLE `skill_level`
+  ADD PRIMARY KEY (`idskill_level`);
+
+--
 -- Indexes for table `skill_person`
 --
 ALTER TABLE `skill_person`
   ADD PRIMARY KEY (`idSkill_person`),
   ADD KEY `fk_skill_person1_idx` (`idPerson`),
-  ADD KEY `fk_skill_person_skill1_idx` (`idSkill`);
+  ADD KEY `fk_skill_person_skill1_idx` (`idSkill`),
+  ADD KEY `fk_skill_person_skill_level1_idx` (`idskill_level`);
 
 --
 -- Indexes for table `user`
@@ -497,6 +632,21 @@ ALTER TABLE `job_title`
 ALTER TABLE `person`
   MODIFY `idPerson` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `person_contact_mode`
+--
+ALTER TABLE `person_contact_mode`
+  MODIFY `idcontactPreference` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `person_status`
+--
+ALTER TABLE `person_status`
+  MODIFY `idperson_status` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `person_title`
+--
+ALTER TABLE `person_title`
+  MODIFY `idperson_title` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `professional_qualification`
 --
 ALTER TABLE `professional_qualification`
@@ -506,6 +656,11 @@ ALTER TABLE `professional_qualification`
 --
 ALTER TABLE `referee`
   MODIFY `idReferee` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `referee_relationship`
+--
+ALTER TABLE `referee_relationship`
+  MODIFY `idreferee_relationship` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `sector`
 --
@@ -517,15 +672,15 @@ ALTER TABLE `sector`
 ALTER TABLE `skill`
   MODIFY `idSkill` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `skill_person`
+-- AUTO_INCREMENT for table `skill_level`
 --
-ALTER TABLE `skill_person`
-  MODIFY `idSkill_person` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `skill_level`
+  MODIFY `idskill_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idUser` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user_type`
 --
@@ -568,6 +723,9 @@ ALTER TABLE `job_title`
 --
 ALTER TABLE `person`
   ADD CONSTRAINT `fk_person_employment_level1` FOREIGN KEY (`idLevelOfEmployment`) REFERENCES `employment_level` (`idLevelOfEmployment`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_person_person_contact_mode1` FOREIGN KEY (`idcontactPreference`) REFERENCES `person_contact_mode` (`idcontactPreference`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_person_person_status1` FOREIGN KEY (`idperson_status`) REFERENCES `person_status` (`idperson_status`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_person_person_title1` FOREIGN KEY (`idperson_title`) REFERENCES `person_title` (`idperson_title`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_person_user1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -581,14 +739,16 @@ ALTER TABLE `professional_qualification`
 -- Constraints for table `referee`
 --
 ALTER TABLE `referee`
-  ADD CONSTRAINT `fk_referee_person1` FOREIGN KEY (`idPerson`) REFERENCES `person` (`idPerson`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_referee_person1` FOREIGN KEY (`idPerson`) REFERENCES `person` (`idPerson`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_referee_referee_relationship1` FOREIGN KEY (`idreferee_relationship`) REFERENCES `referee_relationship` (`idreferee_relationship`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `skill_person`
 --
 ALTER TABLE `skill_person`
   ADD CONSTRAINT `fk_skill_person1` FOREIGN KEY (`idPerson`) REFERENCES `person` (`idPerson`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_skill_person_skill1` FOREIGN KEY (`idSkill`) REFERENCES `skill` (`idSkill`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_skill_person_skill1` FOREIGN KEY (`idSkill`) REFERENCES `skill` (`idSkill`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_skill_person_skill_level1` FOREIGN KEY (`idskill_level`) REFERENCES `skill_level` (`idskill_level`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `user`
